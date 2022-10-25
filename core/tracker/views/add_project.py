32b3b0937ffcb013f3fe4dd4d394f0838e970d6c
Almukhan Chingiz <chingizpain@gmail.com>
2022-10-25 18:31:07 +0600

    Изменил классовое представления для добавления проекта, при добавлении проставляется юзер создавший проект Обновил фикстуры

--- core/tracker/views/add_project.py
+++ core/tracker/views/add_project.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
@@ -9,9 +10,16 @@ from tracker.views import CustomUserPassesMixin
 
 class AddProjectView(CustomUserPassesMixin, LoginRequiredMixin, CreateView):
     template_name = 'add_project.html'
-    success_url = reverse_lazy('projects')
+    # success_url = reverse_lazy('projects')
     model = Project
     form_class = AddProjectForm
     groups = ['manager', 'root']
+
+    def get_success_url(self):
+        set_user = get_object_or_404(Project, pk=self.object.pk)
+        set_user.users.add(self.request.user)
+        set_user.save()
+        return reverse_lazy('projects')
+
 
 
